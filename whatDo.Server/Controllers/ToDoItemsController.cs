e0bc7fe1d255541083bebdeda5cb7e6f72f17bab
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-11-29 22:07:54 +0330

    add controller endpoints and add project directory for clean architecture

---
+++ whatDo.Server/Controllers/ToDoItemsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using whatDo.Server.Data.Contexts;
+using whatDo.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhatDo.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoItemsController : Controller
+    {
+        private readonly WhatDoDbContext _context;
+
+        public ToDoItemsController(WhatDoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ToDoItem>>> GetToDoItems()
+        {
+            var toDoItems = await _context.ToDoItems.ToListAsync();
+            if (toDoItems == null) return NotFound();
+            return Ok(toDoItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(int id)
+        {
+            var toDoItem = await _context.ToDoItems.FindAsync(id);
+            if (toDoItem == null) return NotFound();
+            return Ok(toDoItem);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem)
+        {
+            _context.ToDoItems.Add(toDoItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetToDoItem), new { id = toDoItem.Id }, toDoItem);
+        }
+
+
+    }
+}
--- whatDo.Server/DTOs/ToDoItemDto.cs
+++ whatDo.Server/DTOs/ToDoItemDto.cs
@@ -1,12 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace whatDo.Server.Models
+﻿namespace WhatDo.Server.DTOs
 {
-    public class ToDoItem
+    public class ToDoItemDto
     {
-        public int Id { get; set; }
-
-        [Required]
         public string? Name { get; set; }
         public string? Description { get; set; }
         public DateTime DueDate { get; set; }
