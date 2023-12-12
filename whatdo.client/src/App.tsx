5f1cf37eab97854166a01b33e613d118d18b5f9e
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-12-12 16:48:07 +0330

    add styling

--- whatdo.client/src/App.tsx
+++ whatdo.client/src/App.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { List, ListItem, ListItemText, Typography, Paper } from '@mui/material';
 
 interface ToDoItem {
     id: number;
@@ -26,15 +27,24 @@ function App() {
 
 
     return (
-        <div>
-            <ul>
-                {toDoItems.map(item => (
-                    <li key={item.id}>
-                        {item.description}
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <Paper elevation={3} style={{ padding: '16px', maxWidth: '600px', margin: 'auto', textAlign: 'center' }}>
+                <Typography variant="h4" gutterBottom>
+                    Todo List
+                </Typography>
+                <List>
+                    {toDoItems.map((ToDoItem) => (
+                        <ListItem key={ToDoItem.id}>
+                            <ListItemText
+                                primary={ToDoItem.name}
+                                secondary={ToDoItem.description}
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+            </Paper>
+            
+        </>
     );
 
     async function getToDoItems() {
